61846f8224cff62d5d3084b3b1ecbed3820d2323
Giammarco Senatore <giammasena@gmail.com>
2020-09-13 14:50:08 +0200

    mido: switch to coral fp to bypass safetynet

--- prop.mk
+++ prop.mk
@@ -7,6 +7,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 dalvik.vm.dex2oat-filter=speed \
 dalvik.vm.image-dex2oat-filter=speed
 
+# Pixel identification
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.build.fingerprint=google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=2 \
