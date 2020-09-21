77a535321b73a8620c4975d9204c33edb36a5378
Basil <basiljohn821@gmail.com>
2020-09-21 16:13:10 +0100

    Get Resurrected

---
+++ prop.mk
@@ -0,0 +1,351 @@
+#
+# system.prop for mido
+#
+
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+af.fast_track_multiplier=2 \
+audio.offload.disable=true \
+audio.offload.min.duration.secs=30 \
+audio.offload.video=true \
+persist.audio.dirac.speaker=true \
+persist.vendor.audio.fluence.audiorec=true \
+persist.vendor.audio.fluence.speaker=true \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=true \
+persist.vendor.audio.hw.binder.size_kbyte=1024 \
+persist.vendor.btstack.enable.splita2dp=false \
+ro.config.media_vol_steps=25 \
+ro.config.vc_call_vol_steps=7 \
+ro.vendor.audio.sdk.fluencetype=fluence \
+ro.vendor.audio.sdk.ssr=false \
+vendor.audio.flac.sw.decoder.24bit=true \
+vendor.audio.offload.buffer.size.kb=64 \
+vendor.audio.offload.gapless.enabled=true \
+vendor.audio.offload.multiaac.enable=true \
+vendor.audio.offload.multiple.enabled=false \
+vendor.audio.offload.track.enable=true \
+vendor.audio.parser.ip.buffer.size=0 \
+vendor.audio.playback.mch.downsample=true \
+vendor.audio.pp.asphere.enabled=false \
+vendor.audio.safx.pbe.enabled=true \
+vendor.audio.tunnel.encode=false \
+vendor.audio.use.sw.alac.decoder=true \
+vendor.audio.use.sw.ape.decoder=true \
+vendor.audio_hal.period_size=192 \
+vendor.voice.conc.fallbackpath=deep-buffer \
+vendor.voice.path.for.pcm.voip=true \
+vendor.voice.playback.conc.disabled=true \
+vendor.voice.record.conc.disabled=false \
+vendor.voice.voip.conc.disabled=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+bluetooth.hfp.client=1 \
+vendor.qcom.bluetooth.soc=smd \
+ro.bluetooth.hfp.ver=1.7
+
+# Boot
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.vendor.shutdown.waittime=500
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+camera.display.lmax=1280x720 \
+camera.display.umax=1920x1080 \
+camera.lowpower.record.enable=1 \
+media.camera.ts.monotonic=1 \
+persist.camera.gyro.disable=0 \
+persist.camera.isp.clock.optmz=0 \
+persist.camera.stats.test=5 \
+persist.vendor.qti.telephony.vt_cam_interface=1 \
+vidc.enc.dcvs.extra-buff-count=2
+
+# Cne
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.cne.feature=1
+
+# Coresight
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.debug.coresight.config=stm-events
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.egl.hw=0 \
+debug.enable.sglscale=1 \
+debug.gralloc.enable_fb_ubwc=1 \
+debug.mdpcomp.logs=0 \
+debug.sf.enable_hwc_vds=1 \
+debug.sf.hw=1 \
+debug.sf.latch_unsignaled=1 \
+debug.composition.type=gpu \
+debug.enabletr=true \
+debug.overlayui.enable=1 \
+debug.performance.tuning=1 \
+hw3d.force=1 \
+hwui.disable_vsync=true \
+hwui.render_dirty_regions=false \
+persist.sys.composition.type=gpu \
+persist.sys.ui.hw=1 \
+ro.config.enable.hw_accel=true \
+ro.product.gpu.driver=1 \
+ro.fb.mode=1 \
+ro.vold.umsdirtyratio=60 \
+ro.sf.compbypass.enable=0 \
+video.accelerate.hw=1 \
+com.qc.hardware=true \
+debug.qc.hardware=true \
+debug.qctwa.preservebuf=1 \
+debug.qctwa.statusbar=1 \
+debug.sf.recomputecrop=0 \
+debug.sf.enable_gl_backpressure=1 \
+debug.sf.disable_backpressure=1 \
+dev.pm.dyn_samplingrate=1 \
+persist.demo.hdmirotationlock=false \
+persist.hwc.enable_vds=1 \
+persist.hwc.mdpcomp.enable=true \
+ro.opengles.version=196610 \
+ro.qualcomm.cabl=0 \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+debug.sdm.support_writeback=0 \
+ro.vendor.display.cabl=2 \
+sdm.debug.disable_skip_validate=1 \
+vendor.display.enable_default_color_mode=1 \
+vendor.display.disable_skip_validate=1 \
+vendor.gralloc.enable_fb_ubwc=1
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.dpm.feature=0
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+drm.service.enabled=true
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.qfp=false
+
+# Fm
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.fm.transmitter=false \
+vendor.hw.fm.init=0
+
+# Frp
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.frp.pst=/dev/block/bootdevice/by-name/config
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+av.debug.disable.pers.cache=1 \
+debug.stagefright.omx_default_rank.sw-audio=1 \
+debug.stagefright.omx_default_rank=0 \
+media.msm8956hw=0 \
+media.stagefright.audio.sink=280 \
+media.stagefright.thumbnail.prefer_hw_codecs=true \
+mmp.enable.3g2=true \
+vendor.audio.hw.aac.encoder=true \
+vendor.mm.enable.qcom_parser=1048575 \
+vendor.vidc.dec.downscalar_height=1088 \
+vendor.vidc.dec.downscalar_width=1920 \
+vendor.vidc.disable.split.mode=1 \
+vendor.vidc.enc.disable.pq=true \
+vendor.vidc.enc.disable_bframes=1 \
+vendor.video.disable.ubwc=1
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.sys.fw.dex2oat_thread_count=8 \
+ro.vendor.extension_library=libqti-perfd-client.so \
+ro.vendor.qti.sys.fw.bservice_enable=true \
+ro.vendor.qti.am.reschedule_service=true
+
+# Netmgrd
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.use_data_netmgrd=true \
+persist.data.netmgrd.qos.enable=true \
+persist.vendor.data.mode=concurrent
+
+# Nitz
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.radio.nitz_plmn="" \
+persist.vendor.radio.nitz_lons_0="" \
+persist.vendor.radio.nitz_lons_1="" \
+persist.vendor.radio.nitz_lons_2="" \
+persist.vendor.radio.nitz_lons_3="" \
+persist.vendor.radio.nitz_sons_0="" \
+persist.vendor.radio.nitz_sons_1="" \
+persist.vendor.radio.nitz_sons_2="" \
+persist.vendor.radio.nitz_sons_3=""
+
+# Qualcomm
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.qti.va_aosp.support=1 \
+ro.vendor.qti.va_odm.support=1
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+DEVICE_PROVISIONED=1 \
+persist.dbg.volte_avail_ovr=1 \
+persist.dbg.vt_avail_ovr=1 \
+persist.dbg.wfc_avail_ovr=1 \
+persist.radio.multisim.config=dsds \
+persist.sys.fflag.override.settings_network_and_internet_v2=true \
+persist.vendor.radio.apm_sim_not_pwdn=1 \
+persist.vendor.radio.custom_ecc=1 \
+persist.vendor.radio.hw_mbn_update=0 \
+persist.vendor.radio.rat_on=combine \
+persist.vendor.radio.sib16_support=1 \
+ril.subscription.types=NV,RUIM \
+rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+ro.telephony.call_ring.multiple=false \
+ro.telephony.default_network=22,22 \
+persist.vendor.radio.aosp_usr_pref_sel=true \
+ro.telephony.use_old_mnc_mcc_format=true \
+persist.vendor.radio.multisim_switch_support=true \
+service.qti.ims.enabled=1 \
+vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
+
+# SurfaceFlinger
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.protected_contents=true
+
+# Time Services
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.delta_time.enable=true
+
+# Tcp
+PRODUCT_PROPERTY_OVERRIDES += \
+net.tcp.2g_init_rwnd=10
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.use_fifo_ui=1
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+wifi.interface=wlan0
+
+# Wifi-Display
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.wfd.virtual=0
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.usb.config=mtp,adb \
+ro.adb.secure=0 \
+ro.secure=0 \
+ro.debuggable=1
+
+# Dirac
+persist.dirac.acs.controller=qem \
+persist.dirac.acs.storeSettings=1 \
+persist.dirac.acs.ignore_error=1 \
+ro.audio.soundfx.dirac=true \
+persist.audio.dirac.speaker=true
+# Dirac algo tsx 9/12
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dirac.acs.controller=qem \
+    persist.dirac.acs.storeSettings=1 \
+    persist.dirac.acs.ignore_error=1
+
+# Dirac headset effect
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.soundfx.dirac=true \
+    persist.audio.dirac.speaker=true
+
+# Improve touch detection
+PRODUCT_PROPERTY_OVERRIDES += \
+    touch.pressure.scale=0.001 \
+    persist.sys.ui.hw=1 \
+    view.scroll_friction=10 \
+    touch.size.calibration=diameter \
+    touch.size.scale=1 \
+    touch.size.bias=0 \
+    touch.size.isSummed=0 \
+    touch.pressure.scale=0.001 \
+    touch.orientation.calibration=none \
+    touch.distance.calibration=none \
+    touch.distance.scale=0 \
+    touch.coverage.calibration=box \
+    touch.gestureMode=spots \
+    MultitouchSettleInterval=1ms \
+    MultitouchMinDistance=1px \
+    TapInterval=1ms \
+    TapSlop=1px
+
+# Spectrum
+PRODUCT_PROPERTY_OVERRIDES += \
+    spectrum.support=1 \
+    persist.spectrum.kernel=Parallax
+
+# Zygote preforking
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
+
+# QC framework value-adds
+ro.vendor.qti.va_aosp.support=1
+    ro.netflix.bsp_rev=Q660-13149-1
+
+# Dirac algo tsx 9/12
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dirac.acs.controller=qem \
+    persist.dirac.acs.storeSettings=1 \
+    persist.dirac.acs.ignore_error=1
+
+# Dirac headset effect
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.audio.soundfx.dirac=true \
+    persist.audio.dirac.speaker=true
+# Fling Velocity
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.min.fling_velocity=160 \
+ro.max.fling_velocity=20000
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
+
+# Spectrum
+PRODUCT_PROPERTY_OVERRIDES += \
+    spectrum.support=1 \
+    persist.spectrum.kernel=Parallax
+
+# Fling Velocity
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.min.fling_velocity=160 \
+ro.max.fling_velocity=20000
+
+# Zygote preforking
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.device_config.runtime_native.usap_pool_enabled=true
+
+# Gboard
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.com.google.ime.corner_key_r=35 \
+ro.com.google.ime.kb_pad_port_b=1
---
+++ rootdir/Android.mk
@@ -0,0 +1,143 @@
+LOCAL_PATH := $(call my-dir)
+
+# Init scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.target.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
+
+# Configuration scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.early_boot.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.early_boot.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.post_boot.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.post_boot.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.goodix.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.goodix.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.spectrum.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.spectrum.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.spectrum.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.gpuboost.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.gpuboost.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.gpuboost.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.gpuboost.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.cpuboost.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.cpuboost.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.cpuboost.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.cpuboost.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.parallax.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.parallax.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.parallax.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.parallax.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
